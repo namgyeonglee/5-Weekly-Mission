a6e5c332592bc6bd9f6ec5eb54818514de8307c9
Erin <skaurd12323@gmail.com>
2024-03-11 16:52:44 +0900

    Fix: 에러메시지가 없어지지 않고 쌓이는 오류 해결

--- js/sign.js
+++ js/sign.js
@@ -1,17 +1,19 @@
+// *---* 이메일 에러메시지 *---* //
+
 const inputEmail = document.querySelector('.input-email');
 const emailInput = document.querySelector('.email');
-const inputErrorMessage = document.querySelector('.input-error-message');
-const formatErrorMessage = document.querySelector('.format-error-message');
+const emailErrorMessage = document.querySelector('.email-error-message');
+
+const emailP = document.createElement('p');
+emailP.classList.add('email-error-message');
 
 // 이메일 값 입력했는지 확인하는 함수
 function validateEmailInput() {
   if (!emailInput.value) {
-    if (!inputErrorMessage) {
+    if (!emailErrorMessage) {
       // 박스 아래 에러 메시지 노출
-      const p = document.createElement('p');
-      p.classList.add('input-error-message');
-      p.textContent = '이메일을 입력해 주세요.';
-      inputEmail.appendChild(p);
+      emailP.textContent = '이메일을 입력해 주세요.';
+      inputEmail.appendChild(emailP);
 
       // 박스 테두리 빨간색으로 변경
       emailInput.classList.add('error-border');
@@ -20,9 +22,8 @@ function validateEmailInput() {
     // 박스 테두리 빨간색으로 변경하는 클래스 제거
     emailInput.classList.remove('error-border');
 
-    if (inputErrorMessage) {
-      inputEmail.removeChild(inputErrorMessage);
-    }
+    // p 태그 내 에러메시지 문자열 제거
+    emailP.textContent = '';
   }
 }  
 
@@ -41,12 +42,10 @@ function validateEmailFormat() {
 
   // 에러 메시지 처리
   if (!isValidEmail) {
-    if (!formatErrorMessage) {
+    if (!emailErrorMessage) {
       // 박스 아래 에러 메시지 노출
-      const p = document.createElement('p');
-      p.classList.add('format-error-message');
-      p.textContent = '올바른 이메일 주소가 아닙니다.';
-      inputEmail.appendChild(p);
+      emailP.textContent = '올바른 이메일 주소가 아닙니다.';
+      inputEmail.appendChild(emailP);
 
       // 박스 테두리 빨간색으로 변경
       emailInput.classList.add('error-border');
@@ -55,30 +54,38 @@ function validateEmailFormat() {
     // 박스 테두리 빨간색으로 변경하는 클래스 제거
     emailInput.classList.remove('error-border');
 
-    if (formatErrorMessage) {
-      inputEmail.removeChild(formatErrorMessage);
-    }
+    // p 태그 내 에러메시지 문자열 제거
+    emailP.textContent = '';
   }
+}
+
+// 포커스 인 되면 에러메시지 리셋하는 함수
+function resetEmailErrorMessage() {
+  emailP.textContent = '';
 }
 
 emailInput.addEventListener('focusout', validateEmailInput);
 emailInput.addEventListener('focusout', validateEmailFormat);
+emailInput.addEventListener('focusin', resetEmailErrorMessage);
 
 
+
+// *---* 패스워드 에러메시지 *---* //
 
 const inputPassword = document.querySelector('.input-password');
 const passwordInput = document.querySelector('.password');
-const passwordInputErrorMessage = document.querySelector('.password-input-error-message');
+const passwordErrorMessage = document.querySelector('.password-error-message');
+
+const passwordP = document.createElement('p');
+passwordP.classList.add('password-error-message');
 
 // 비밀번호 값 입력했는지 확인하는 함수
 function validatePasswordInput() {
   if (!passwordInput.value) {
-    if (!passwordInputErrorMessage) {
+    if (!passwordErrorMessage) {
       // 박스 아래 에러 메시지 노출
-      const p = document.createElement('p');
-      p.classList.add('password-input-error-message');
-      p.textContent = '비밀번호를 입력해 주세요.';
-      inputPassword.appendChild(p);
+      passwordP.textContent = '비밀번호를 입력해 주세요.';
+      inputPassword.appendChild(passwordP);
 
       // 박스 테두리 빨간색으로 변경
       passwordInput.classList.add('error-border');
@@ -86,15 +93,23 @@ function validatePasswordInput() {
   } else {
     // 박스 테두리 빨간색으로 변경하는 클래스 제거
     passwordInput.classList.remove('error-border');
-
-    if (passwordInputErrorMessage) {
-      inputPassword.removeChild(passwordInputErrorMessage);
-    }
+    
+    // p 태그 내 에러메시지 문자열 제거
+    passwordP.textContent = '';
   }
 }  
 
-passwordInput.addEventListener('focusout', validatePasswordInput);
+// 포커스 인 되면 에러메시지 리셋하는 함수
+function resetPasswordErrorMessage() {
+  passwordP.textContent = '';
+}
 
+passwordInput.addEventListener('focusout', validatePasswordInput);
+passwordInput.addEventListener('focusin', resetPasswordErrorMessage);
+
+
+
+// *---* 로그인 시도 에러메시지 *---* //
 
 // test ID/PW와 일치할 경우 로그인 버튼 클릭 시 folder 페이지로 이동
 
@@ -104,6 +119,20 @@ function goFolder() {
   if (emailInput.value === "test@codeit.com" && passwordInput.value === "codeit101") {
     let link = '/folder';
     location.href = link;
+  } else { 
+    // 이외의 이메일 ID 입력 후 로그인 버튼 클릭 시 에러 메시지 노출
+    emailP.textContent = '이메일을 확인해 주세요.';
+    inputEmail.appendChild(emailP);
+    
+    // 이메일 박스 테두리 빨간색으로 변경
+    emailInput.classList.add('error-border');
+
+    // 이외의 패스워드 입력 후 로그인 버튼 클릭 시 에러 메시지 노출
+    passwordP.textContent = '비밀번호를 확인해 주세요.';
+    inputPassword.appendChild(passwordP);
+
+    // 패스워드 박스 테두리 빨간색으로 변경
+    passwordInput.classList.add('error-border');
   }
 }
 
